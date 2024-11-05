6a5b79c3050570236d7afc914ad8b51fc6662a7f
Nicolas Mema <nicolas.a.mema@gmail.com>
2024-11-05 20:51:00 -0300

    #5 move external data sources

--- infra/api/lambda.tf
+++ infra/api/lambda.tf
@@ -1,23 +1,3 @@
-data "terraform_remote_state" "db" {
-  backend = "s3"
-
-  config = {
-    bucket = "terraform-baby-state-dev"
-    key    = "data-stores/dynamodb/terraform.tfstate"
-    region = "us-west-2"
-  }
-}
-
-data "terraform_remote_state" "cognito" {
-  backend = "s3"
-
-  config = {
-    bucket = "terraform-baby-state-dev"
-    key    = "cognito/terraform.tfstate"
-    region = "us-west-2"
-  }
-}
-
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
